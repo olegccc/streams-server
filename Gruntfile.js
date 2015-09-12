78da978c6688ca91b69b32ee17fad5de003ca195
olegccc <oleg@gordeev.cz>
2015-09-12 18:32:17 +0300

    prepared dev env

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.config.init(grunt.file.readJSON('./config/grunt.config.json'));
+    grunt.task.loadTasks('grunt-tasks');
+
+    grunt.registerTask('jasmine', function() {
+        var done = this.async();
+        var Jasmine = require('jasmine');
+        var jasmine = new Jasmine();
+        jasmine.loadConfigFile('config/jasmine.config.json');
+        jasmine.onComplete(function(passed) {
+            if (!passed) {
+                grunt.log.error('Jasmine specs failed');
+            }
+            done();
+        });
+        jasmine.execute();
+    });
+
+    grunt.registerTask('build', ['ts:build']);
+    grunt.registerTask('run', []);
+    grunt.registerTask('test', ['build', 'jasmine']);
+};
---
+++ src/specs/simple.spec.ts
@@ -0,0 +1,5 @@
+describe('Test suite', () => {
+    it('contains expectation', () => {
+        expect(true).toBeTruthy();
+    });
+});
